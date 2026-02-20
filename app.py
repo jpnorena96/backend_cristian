e41a2357d649dcbf808f30bac0061441ea8aa322
jpna <jpnorena96@gmail.com>
2026-02-20 17:57:00 -0500

    finalizeds

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5173", "https://iuristatech.com", "https://www.iuristatech.com"]}})
     db.init_app(app)
 
     from admin_routes import admin_bp
